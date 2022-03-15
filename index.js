f8131917fcc1065b2c241a0864c60282379b2689
DennisHvA <dennis.de.graaf2@hva.nl>
2022-03-15 14:41:59 +0100

    not working

--- index.js
+++ index.js
@@ -1,87 +1,83 @@
-// require express
-const express = require('express')
-// set app equal to the execution of express
-const app = express()
-// we need bodyparser to extract data from the client
-const bodyParser = require('body-parser')
+// require
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const slug = require('slug');
+const multer = require('multer');
+const { MongoClient } = require('mongodb');
+require('dotenv').config();
+const { engine } = require('express-handlebars');
 
-const slug = require('slug')
-const multer = require('multer')
-// require('dotenv').config()
+// port
+const PORT = process.env.PORT || 1337;
 
-const { MongoClient } = require("mongodb");
-require("dotenv").config();
 let database = null;
 
-// we need a view engine to render template to the client
-const { engine } = require('express-handlebars')
-// we need a port for our server to listen to
-const PORT = process.env.PORT || 1337
-// here I specify my templating logic and view engine
-app.engine('handlebars', engine())
-app.set('view engine', 'handlebars')
-app.set('views', './views')
-// here I use bodyparser to extract the data from the client
-app.use(bodyParser.urlencoded({extended: false}))
-app.use(bodyParser.json())
-app.use(express.static('public'))
-// this is the home route
-app.get('/', async function(req, res, next) {
-  let user = await database
+// handlebars
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', './views');
+
+// bodyparser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static('public'));
+
+// routes
+app.get('/', async (req, res, next) => {
+  const user = await database.collection('users').findOne({ email: 'test' });
+  console.log(user);
+  res.render('home');
+});
+
+app.get('/login', (req, res, next) => {
+  res.render('login');
+});
+
+app.get('/ingelogd', (req, res, next) => {
+  res.render('ingelogd');
+});
+
+app.get('/register', (req, res, next) => {
+  res.render('register');
+});
+
+app.get('/aangemaakt', (req, res, next) => {
+  res.render('aangemaakt');
+});
+
+app.post("/aangemaakt", async (req, res) => {
+  await database
     .collection("users")
-    .findOne({ name: "Dennis" });
-  console.log(user)
-  res.render('home')
-})
+    .insertOne({ email: req.body.email, password: req.body.password });
 
-// this is the login route
-app.get('/login', function(req, res, next) {
-  res.render('login')
-})
+  let newUser = await database
+    .collection("users")
+    .findOne({ email: req.body.email });
 
-// this is the register route
-app.get('/register', function(req, res, next) {
-  res.render('register')
-})
+  console.log(newUser); // Log the new user to the database
 
-// app.post('/getData', )
+  res.redirect("/");
+});
 
-// this function inits my server to listen to: localhost:PORT 
-app.listen(PORT, () => {
-  console.log('App is listening to port', PORT)
-  connectDB().then(() => {
-    console.log("Connected to MongoDB");
-  });
-})
-
+// database
 async function connectDB() {
-
-  // Connection URL from .env file
-
   const client = new MongoClient(process.env.DB_URL, {
-
     retryWrites: true,
-
     useUnifiedTopology: true,
-
     useNewUrlParser: true,
-
   });
-
   try {
-
-    await client.connect(); // Connect the client
-
-    database = client.db(process.env.DB_NAME); // Get the database from the client
-
+    await client.connect();
+    database = client.db(process.env.DB_NAME);
   } catch (error) {
-
     console.log(error);
-
   }
-
 }
 
-// app.post('/register', function(req, res, next) {
-
-// }
+app.listen(PORT, () => {
+  console.log('App is listening to port', PORT);
+  connectDB().then(() => {
+    console.log('Connected to MongoDB');
+  });
+});
